2277c7ae5f9eba27edaba37bf85194a87527f5dd
annguyen36 <37259991+annguyen36@users.noreply.github.com>
2023-11-16 07:00:34 +0000

    adding some fiirst book title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { 
+  constructor() {
+    super();
+    this.state = {
+      books: [
+        {
+          name: "Start With Why",
+          ISBN: "1234567890"
+        },
+        {
+          name: "Leader eat last",
+          ISBN: "0987654321"
+        },
+        {
+          name: "Sweet Little lies",
+          ISBN: "23457890"
+        }
+      ]
+      
+    }
+  }
+  render(){
+    return (
+      <div className='App'>
+        {
+          this.state.books.map((book) => {
+            return <h1 key={book.ISBN}>{book.name}</h1>
+          })
+        }
+      
+      </div>
+    )
+  }
 }
 
 export default App;

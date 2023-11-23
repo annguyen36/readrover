ce8ecdad09d7ad54779d48e71e828d46531de73e
An Nguyen <nguanv@amazon.com>
2023-11-22 22:56:52 -0800

    Add some styling and picture

--- src/components/card-list/card-list.jsx
+++ src/components/card-list/card-list.jsx
@@ -1,13 +1,29 @@
 import { Component } from "react";
+import './card-list.styles.css';
 
+const covers = require.context('./book-cover', true)
+
+const cover_list = covers.keys().map(image => covers(image));
+console.log(cover_list)
 class CardList extends Component {
     render(){
         const books = this.props.books;
         return(
-            <div>
+            <div className='card-list'>
             {
                 books.map((book) => {
-                  return <h1>{book.name}</h1>
+                    
+                  return (
+                    <div className='card-container'>
+                        <h1>{book.name}</h1>
+                        <h1>{book.id}</h1>
+                        <img alt={book.name} src={`1.jpg`}/>
+                    </div>
+                    
+                    
+                    )
+                  
+                    
                 })
             }
             </div>  
